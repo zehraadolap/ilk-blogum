1cf156a583893894b61102995236588ca1df6219
Zehra Dolap <zehradolap@anadolu.edu.tr>
2017-04-08 15:15:44 +0300

    Detaylı blog gönderileri için CSS'e ilaveten view ve template eklendi.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your viewshere.
 def post_list(request):
 
     posts=Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
